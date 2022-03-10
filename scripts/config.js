68eb5f5ac55ef8e3f3ecdf18d354b004fa23572f
Colintate24 <Colingarrett24@yahoo.com>
2022-03-10 12:06:11 -0600

    update config

--- scripts/config.js
+++ scripts/config.js
@@ -39,4 +39,129 @@ export const games = [
     link: "https://almostheaven52.github.io/dogey-howser/",
     author: "Zach Newell",
   },
+  {
+    title: "Delta",
+    link:  "https://natalie-daniel.github.io/DELTA/",
+    author: "Natalie Daniel",
+  },
+  {
+    title: "WowGame",
+    link:  "https://natalie-daniel.github.io/wowgame/",
+    author: "Natalie Daniel",
+  },
+  {
+    title: "Studious Waffle",
+    link:  "https://studentzilla.github.io/studious-waffle/",
+    author: "StudentZilla",
+  },
+  {
+    title: "Fuzzy Computing Machine",
+    link:  "https://cdman814.github.io/fuzzy-computing-machine/",
+    author: "CDman814",
+  },
+  {
+    title: "Vigalante Octo Parakeet",
+    link:  "https://cdman814.github.io/vigilant-octo-parakeet/",
+    author: "CDman814",
+  },
+  {
+    title: "FrostGames",
+    link:  "https://frosty9424.github.io/frostgames/",
+    author: "Frosty9424",
+  },
+  {
+    title: "Upgraded Bassoon",
+    link:  "https://gatordj98.github.io/upgraded-bassoon/",
+    author: "GatorDJ98",
+  },
+  {
+    title: "Symmetrical Fiesta",
+    link:  "https://gatordj98.github.io/symmetrical-fiesta/",
+    author: "GatorDJ98",
+  },
+  {
+    title: "Friendly Computing Machine",
+    link:  "https://lterwilliger.github.io/friendly-computing-machine/",
+    author: "Lterwilliger",
+  },
+  {
+    title: "Urban Train",
+    link:  "https://lterwilliger.github.io/urban-train/",
+    author: "Lterwilliger",
+  },
+  {
+    title: "Shiny Shinee",
+    link:  "https://laurenmalone01.github.io/shiny-shinee/",
+    author: "BigDaddy Malone",
+  },
+  {
+    title: "Upgraded Memory",
+    link:  "https://laurenmalone01.github.io/upgraded-memory/",
+    author: "BigDaddy Malone",
+  },
+  {
+    title: "Edgy Bourban",
+    link:  "https://errorless-stone.github.io/edgy-bourbon/",
+    author: "Errorless-Stone",
+  },
+  {
+    title: "Cumbie-Delta",
+    link:  "https://errorless-stone.github.io/cumbie-delta/",
+    author: "Errorless-Stone",
+  },
+  {
+    title: "Extinction-Party",
+    link: "https://akennemer.github.io/extinction-party/",
+    author: "Ashe Kennemer",
+  },
+  {
+    title: "Teen Idle",
+    link: "https://akennemer.github.io/teen-idle/",
+    author: "Ashe Kennemer",
+  },
+  {
+    title: "CharlieCode",
+    link: "https://cgooch99.github.io/CharlieCode/",
+    author: "Cgooch99",
+  },
+  {
+    title: "Juicy Fruit",
+    link: "https://cgooch99.github.io/juicy-fruit/",
+    author: "Cgooch99",
+  },
+  {
+    title: "LabCharlie",
+    link: "https://peytonjunjulas.github.io/labCharlie/",
+    author: "PeytonJunjulas",
+  },
+  {
+    title: "Lab Delta",
+    link: "https://peytonjunjulas.github.io/lab-Delta/",
+    author: "PeytonJunjulas",
+  },
+  {
+    title: "Raney Lab Echo",
+    link: "https://gavinraney.github.io/raney-lab-echo/",
+    author: "Gavin Raney",
+  },
+  {
+    title: "Raney Lab Delta",
+    link: "https://gavinraney.github.io/raney-lab-delta/",
+    author: "Gavin Raney",
+  },
+  {
+    title: "Scaling Palm Tree",
+    link: "https://kobyingram11.github.io/scaling-palm-tree/",
+    author: "KobyIngram11",
+  },
+  {
+    title: "Expert Computing Machine",
+    link: "https://jacobsanders7.github.io/expert-computing-machine/",
+    author: "Jacobsanders7",
+  },
+  {
+    title: "Crispy Telegram",
+    link: "https://jacobsanders7.github.io/crispy-telegram/pages/charlie-wow.html",
+    author: "Jacobsanders7",
+  },
 ];
