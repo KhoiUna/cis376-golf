e5ca0ddf2856b0bfe16bb33812cc48da7338d7a3
KhoiNguyen <thorwaitson@gmail.com>
2022-03-10 11:07:12 -0600

    add config.js

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -1,37 +1,6 @@
-$(function () {
-  const games = [
-    {
-      title: "Charlie Wow",
-      link: "https://colintate24.github.io/lab-charlie/",
-      author: "Colin Tate",
-    },
-    {
-      title: "Cool Name Facts",
-      link: "https://colintate24.github.io/scaling-tribble/",
-      author: "Colin Tate",
-    },
-    {
-      title: "Khoi's Wow",
-      link: "https://khoiuna.github.io/khoi_wow_meme/",
-      author: "Khoi Nguyen",
-    },
-    {
-      title: "Echo Lab",
-      link: "https://khoiuna.github.io/cis-echo-lab/",
-      author: "Khoi Nguyen",
-    },
-    {
-      title: "Professor-Doge",
-      link: "https://almostheaven52.github.io/Professor-Doge/",
-      author: "Zach Newell"
-    },
-    {
-      title: "Dogey-Howser",
-      link: "https://almostheaven52.github.io/dogey-howser/",
-      author: "Zach Newell"
+import { games } from "./config.js";
 
-    }
-  ];
+$(function () {
   // iterate over the JSON array
   $.each(games, function (index, item) {
     console.log("Game Title: " + item.title);
